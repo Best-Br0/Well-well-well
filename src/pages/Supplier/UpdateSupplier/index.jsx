7ecb3a87699ea723d9f40cd3fae11b56138f3c7d
umarovali <umarovali.345@gmail.com>
2026-05-22 09:19:24 +0600

    confflict

---
+++ src/pages/Supplier/UpdateSupplier/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UpdateSuppiler from './components'
+import Header from '../../../components/Header'
+
+export default function UpdateSuppilerPage() {
+    return (
+        <section>
+            <Header title="Изменить поставшика" />
+            <UpdateSuppiler />
+        </section>
+    )
+}
