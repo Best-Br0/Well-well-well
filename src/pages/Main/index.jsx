4885e746a94b6d653c3b0ea2f5ce579d038403ea
umarchik000 <rasulovumar20102010@gmail.com>
2026-05-22 15:25:09 +0600

    header

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,9 +1,12 @@
+import Header from "../../components/Header";
 
 export default function Main() {
 
     return (
         <>
-        <h1>Main</h1>
+        <Header title={"Главная"} />
+
+        / ваша страница
         </>
     )
 }
